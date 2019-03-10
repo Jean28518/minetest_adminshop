30474f6b757cfcb4788d815394838b02bdbb2bdc
Jean <martin.zie@gmx.de>
2019-03-10 21:53:48 +0100

    Adding atm support

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Github: https://github.com/Jean28518/minetest_adminshop
 
 Mod for Minetest, which adds adminshops.
 
-There is no crafting recipe available.
-To place and to break adminshops you need the "adminshop" privilege assigned.
+There are no crafting recipe available.
+To place and to break adminshops you need the "adminshop" privilege assigned. Only the owner (placer) of the adminshop is able to change the offer.
 
-## How to work with licenses and adminshop
+## Optional: How to work with licenses and adminshop
 The Mod can be found at: https://github.com/Jean28518/minetest_licenses
 
 When the Mod activated you can find a new Item called "Adminshop with licenses integrated".
@@ -14,9 +14,18 @@ When the licenses list in the shop window is empty, everyone can buy something
 from the shop.
 
 You can add a license, by typing the license in the textfield, and clicking to
-"Add". Please remind that this license has to be defined before by `licenses_add LICENSE`.
+"Add". Please remind that this license has to be defined before by `/licenses_add LICENSE`.
+
+You remove a license from the list by typing the name in the textfield and clicking "Remove"
 
 When there are some licenses in the license list in the shop window, then **only
 players can buy**, whose are **having at least one license assigned, which was
 defined in the shop window** by the owner. Only the owner of the shop is able
 to define the required licenses.
+
+## Optional: adminshop with currency and atm
+For that feature you have to installed "licenses" too. This Feature adds an third adminshop, wich takes access to the atm balance of a player.
+
+ATM: https://forum.minetest.net/viewtopic.php?t=15029&p=223265
+
+Currency: https://github.com/minetest-mods/currency
--- init.lua
+++ init.lua
@@ -319,3 +319,271 @@ minetest.register_on_player_receive_fields(function(customer, formname, fields)
 		show_specl(customer)
 	end
 end)
+
+----------------------------------------------------------------------------
+-- Adminshop with licenses and currency with atm support
+----------------------------------------------------------------------------
+if minetest.get_modpath("licenses") ~= nil and minetest.get_modpath("currency") ~= nil and minetest.get_modpath("atm") ~= nil then
+	default.adminshop_current_license_atm_shop_position = {}
+	minetest.register_node("adminshop:adminshop_license_atm", {
+
+	    description = "Adminshop with licenses and atm integrated",
+	    tiles = {"shop_licenses.png"},
+	    is_ground_content = true,
+	    groups = {choppy=2},
+	    sounds = default.node_sound_wood_defaults(),
+	-- Registriere den Owner beim Platzieren:
+	    after_place_node = function(pos, placer, itemstack)
+	      if not minetest.check_player_privs(placer:get_player_name(), { adminshop=true }) then
+	  --    if minetest.check_player_privs(placer:get_player_name(), { adminshop=false }) then
+	        minetest.chat_send_player(placer:get_player_name(),"You don't have the required privilege to place an adminshop!")
+	        minetest.remove_node(pos)
+	      end
+	      local meta = minetest.get_meta(pos)
+				meta:set_string("adminshop:bs", "Buy")
+				meta:set_int("adminshop:price", 0)
+	      meta:set_string("owner", placer:get_player_name())
+	      local inv = meta:get_inventory()
+	      inv:set_size("einnahme", 2*2)
+	      inv:set_size("ausgabe", 2*2)
+	    end,
+	    on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+	      -- Schreibe die eigene Position des Blockes in eine öffentliche Variable mit dem Namen des Spielernamens, welcher auf den Block zugegriffen hat
+	      default.adminshop_current_license_atm_shop_position[player:get_player_name()] = pos
+	      show_specl_atm(player)
+	      --end
+	    end,
+	    allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+	      local meta = minetest.get_meta(pos)
+	      if player:get_player_name() ~= meta:get_string("owner") then return 0 end
+	      return stack:get_count()
+	    end,
+	    allow_metadata_inventory_take = function(pos, listname, index, stack, player)
+	      local meta = minetest.get_meta(pos)
+	      if player:get_player_name() ~= meta:get_string("owner") then return 0 end
+	      return stack:get_count()
+	    end,
+	    can_dig = function(pos, player)
+	      if minetest.check_player_privs(player:get_player_name(), { adminshop=true }) then
+	        return true
+	      else
+	        return false
+	    end
+	  end
+
+
+	})
+
+	show_specl_atm = function (player)
+	    local pos = default.adminshop_current_license_atm_shop_position[player:get_player_name()]
+	    local meta = minetest.get_meta(pos)
+	    local listname = "nodemeta:"..pos.x..','..pos.y..','..pos.z
+			local licenses_string = ""
+			local ltable = minetest.deserialize(meta:get_string("adminshop:ltable"))
+			if ltable == nil then
+				ltable = {}
+			end
+			for k, v in pairs(ltable) do
+				 if v == "defined" then
+					 licenses_string = licenses_string  .. k ..","
+				 end
+			end
+	    if player:get_player_name() == meta:get_string("owner") then
+	     minetest.show_formspec(player:get_player_name(), "adminshop:adminshop_license_atm", "size[11,7.5]"..
+	     "label[0,0;Welcome back, ".. meta:get_string("owner").."]" ..
+			 -- Licenses:
+			 "label[8,0;Licenses:]" ..
+			 "textlist[8,0.5;2.8,5;license_list;"..licenses_string.."]" ..
+			 "button[8,5.5;1.5,1;add_license;Add]" ..
+			 "button[9.5,5.5;1.5,1;remove_license;Remove]" ..
+			 "field[8.3,6.7;3,1;license_text;;]" ..
+			 --for word in string.gmatch("Hello Lua user", "%a+") do print(word) end
+			 -------------
+	     "label[0,0.5;Items:]" ..
+	     "list["..listname..";einnahme;0,1;2,2;]"..
+			 "field[5.4,1.8;2.7,1;price_field;Price:;"..meta:get_string("adminshop:price").."]" ..
+			 "button[5.1,2.3;2.7,1;set_price;Set Price]" ..
+	     "list[current_player;main;0,3.5;8,4;]" ..
+	     "button[3,1.5;2,1;set_buy_sell;"..meta:get_string("adminshop:bs").."]"
+	   )
+	    else
+	      minetest.show_formspec(player:get_player_name(), "adminshop:adminshop_license_atm", "size[8,7.5]"..
+	      "label[0,0;Welcome, "..player:get_player_name().."]" ..
+	      "label[0,0.5;Items:]" ..
+	      "list["..listname..";einnahme;0,1;2,2;]"..
+	      "label[5.7,1.45;Price: "..meta:get_string("adminshop:price").."]" ..
+				"label[5.7,1.8;Your Balance: "..atm.balance[player:get_player_name()].."]" ..
+	      "list[current_player;main;0,3.5;8,4;]" ..
+	      "button[3,1.5;2,1;buy_sell;"..meta:get_string("adminshop:bs").."]"
+	    )
+	  end
+	end
+
+	-- Wenn der Spieler auf Exchange gedrückt hat:
+	minetest.register_on_player_receive_fields(function(customer, formname, fields)
+		if formname == "adminshop:adminshop_license_atm" and fields.buy_sell ~= nil and fields.buy_sell ~= "" then
+	    local pos = default.adminshop_current_license_atm_shop_position[customer:get_player_name()]
+	    local meta = minetest.get_meta(pos)
+	    local minv = meta:get_inventory()
+	    local pinv = customer:get_inventory()
+	    local items = minv:get_list("einnahme")
+	    if items == nil then return end -- do not crash the server
+
+		-- Check if We Can Exchange:
+		local enough_space = true
+		local enough_items = true
+			-- BUY:
+			if meta:get_string("adminshop:bs") == "Buy" then
+				for i, item in pairs(items) do
+					if not pinv:room_for_item("main", item)  then
+						enough_space = false
+					end
+				end
+
+
+
+			-- SELL:
+			else
+				for i, item in pairs(items) do
+					if not pinv:contains_item("main",item) then
+						enough_items = false
+					end
+				end
+			end
+
+
+		-- Check Licence:
+			local allowed = false
+			local empty = true
+			local ltable = minetest.deserialize(meta:get_string("adminshop:ltable"))
+			if ltable == nil then
+				ltable = {}
+			end
+			for k, v in pairs(ltable) do
+				empty = false
+				if v == "defined" then
+					if licenses_check_player_by_licese(customer:get_player_name(), k) then
+						allowed = true
+					end
+				end
+			end
+			if empty then
+				allowed = true
+			end
+		-- Enough Money???
+			if meta:get_string("adminshop:bs") == "Buy" and atm.balance[customer:get_player_name()] < meta:get_int("adminshop:price") then
+				minetest.chat_send_player(customer:get_player_name(),"You dont have enough money on your account!" )
+				return
+			end
+
+		-- Do the thing:
+			-- BUY:
+			if meta:get_string("adminshop:bs") == "Buy" then
+				if enough_space and allowed then
+					for i, item in pairs(items) do
+						pinv:add_item("main",item)
+					end
+					atm.balance[customer:get_player_name()] = atm.balance[customer:get_player_name()] - meta:get_int("adminshop:price")
+					show_specl_atm(customer)
+				elseif not allowed then
+					minetest.chat_send_player(customer:get_player_name(),"You are not allowed to buy this!" )
+				elseif not enough_space then
+					minetest.chat_send_player(customer:get_player_name(),"You don't have enough space in your inventory!")
+				end
+			-- SELL:
+			else
+				if allowed and enough_items then
+					for i, item in pairs(items) do
+				 	 pinv:remove_item("main",item)
+				  end
+					atm.balance[customer:get_player_name()] = atm.balance[customer:get_player_name()] + meta:get_int("adminshop:price")
+					show_specl_atm(customer)
+				elseif not allowed then
+					minetest.chat_send_player(customer:get_player_name(),"You are not allowed to buy this!" )
+				else
+					minetest.chat_send_player(customer:get_player_name(),"You don't have the required items in your inventory!" )
+				end
+
+			end
+			atm.saveaccounts()
+			-- --
+	    -- if enough_space and allowed then
+	    --   for i, item in pairs(items) do
+	    --     pinv:remove_item("main",item)
+	    --   end
+	    --   for i, item in pairs(gives) do
+	    --     pinv:add_item("main",item)
+	    --   end
+	    --   minetest.chat_send_player(customer:get_player_name(),"Exchanged!")
+			-- elseif not allowed then
+			-- 	minetest.chat_send_player(customer:get_player_name(),"You are not allowd to buy this!" )
+			-- elseif enough_space then
+	    --   minetest.chat_send_player(customer:get_player_name(),"You don't have the required items in your inventory!")
+	    -- else
+	    --   minetest.chat_send_player(customer:get_player_name(),"You don't have enough space in your inventory!")
+	    -- end
+	  end
+	end)
+end
+
+-- Add License
+minetest.register_on_player_receive_fields(function(customer, formname, fields)
+	if formname == "adminshop:adminshop_license_atm" and fields.add_license ~= nil and fields.add_license ~= "" then
+    local pos = default.adminshop_current_license_atm_shop_position[customer:get_player_name()]
+    local meta = minetest.get_meta(pos)
+		local ltable = minetest.deserialize(meta:get_string("adminshop:ltable"))
+		if ltable == nil then
+			ltable = {}
+		end
+		-- Checke, ob das wirklich existiert
+    if licenses_exists(fields.license_text) then
+			ltable[fields.license_text] = "defined"
+			meta:set_string("adminshop:ltable", minetest.serialize(ltable))
+			show_specl_atm(customer)
+		else
+			minetest.chat_send_player(customer:get_player_name(),"This license doesnt exist! Add this with /licenses_add " .. fields.license_text )
+		end
+	end
+end)
+
+-- Remove License
+minetest.register_on_player_receive_fields(function(customer, formname, fields)
+	if formname == "adminshop:adminshop_license_atm" and fields.remove_license ~= nil and fields.remove_license ~= "" then
+		local pos = default.adminshop_current_license_atm_shop_position[customer:get_player_name()]
+		local meta = minetest.get_meta(pos)
+		local ltable = minetest.deserialize(meta:get_string("adminshop:ltable"))
+		if ltable == nil then
+			ltable = {}
+		end
+		ltable[fields.license_text] = nil
+		meta:set_string("adminshop:ltable", minetest.serialize(ltable))
+		show_specl_atm(customer)
+	end
+end)
+
+-- Set Buy / Sell
+minetest.register_on_player_receive_fields(function(customer, formname, fields)
+	if formname == "adminshop:adminshop_license_atm" and fields.set_buy_sell ~= nil and fields.set_buy_sell ~= "" then
+		local pos = default.adminshop_current_license_atm_shop_position[customer:get_player_name()]
+		local meta = minetest.get_meta(pos)
+		if meta:get_string("adminshop:bs") == "Buy" then
+			meta:set_string("adminshop:bs", "Sell")
+		else
+			meta:set_string("adminshop:bs", "Buy")
+		end
+
+		show_specl_atm(customer)
+	end
+end)
+
+-- Set Price
+minetest.register_on_player_receive_fields(function(customer, formname, fields)
+	if formname == "adminshop:adminshop_license_atm" and fields.set_price ~= nil and fields.set_price ~= "" then
+		local pos = default.adminshop_current_license_atm_shop_position[customer:get_player_name()]
+		local meta = minetest.get_meta(pos)
+		if tonumber(fields.price_field) ~= nil then
+			meta:set_int("adminshop:price", fields.price_field)
+		end;
+		show_specl_atm(customer)
+	end
+end)
